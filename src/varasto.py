fe589ccbaf6fa6bcb29362efb9de054efe22d0d1
tumffa <tuomas.v.niemi@gmail.com>
2023-11-11 16:01:08 +0200

    Fix varasto

--- src/varasto.py
+++ src/varasto.py
@@ -16,9 +16,7 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea
-    # Ei tarvita erillistä kenttää viela_tilaa tms
-
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
@@ -31,6 +29,9 @@ class Varasto:
             self.saldo = self.tilavuus
 
     def ota_varastosta(self, maara):
+        if maara < 0:
+            return 0.0
+        
         if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
@@ -40,6 +41,7 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
+
 
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
